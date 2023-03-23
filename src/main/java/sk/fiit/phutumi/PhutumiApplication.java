04989aab2fe24d47710801408541ba048b096d05
Withalion <bagarmatej@gmail.com>
2023-03-23 19:02:59 +0100

    Hello world

---
+++ src/main/java/sk/fiit/phutumi/PhutumiApplication.java
@@ -0,0 +1,13 @@
+package sk.fiit.phutumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhutumiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhutumiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/fiit/phutumi/PhutumiApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.fiit.phutumi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhutumiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
