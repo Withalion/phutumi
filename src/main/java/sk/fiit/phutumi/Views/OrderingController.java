020c36507db9939603f97e2e928de91c925fce02
Withalion <bagarmatej@gmail.com>
2023-04-13 14:45:56 +0200

    show resturants on main page

--- src/main/java/sk/fiit/phutumi/Views/OrderingController.java
+++ src/main/java/sk/fiit/phutumi/Views/OrderingController.java
@@ -3,13 +3,19 @@ package sk.fiit.phutumi.Views;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Controller
 public class OrderingController {
+    WebClient client = WebClient.create("http://localhost:8080/phutumi");
 
     @GetMapping("/phutumi")
     public String mainPage(Model model){
-        model.addAttribute("name", "Phutumi");
+        Mono<List> restaurants = client.get().uri("/restaurants").retrieve().bodyToMono(List.class);
+        model.addAttribute("restaurants", restaurants.block());
         return "mainPage";
     }
 }
