ba9ba69eb9ffd8fef00416678f73a0919f64f7fd
xbarantomik <a.baran.tomik@gmail.com>
2023-04-26 23:49:05 +0200

    camunda /food (not tested)

--- src/main/java/sk/fiit/phutumi/controllers/PrintFood.java
+++ src/main/java/sk/fiit/phutumi/controllers/PrintFood.java
@@ -1,49 +1,41 @@
 package sk.fiit.phutumi.controllers;
 
 import javax.inject.Named;
-
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
-import org.camunda.bpm.engine.variable.Variables;
-import org.camunda.bpm.engine.variable.value.ObjectValue;
-import org.camunda.spin.Spin;
-import org.camunda.spin.json.SpinJsonNode;
 import org.camunda.spin.plugin.variable.SpinValues;
 import org.camunda.spin.plugin.variable.value.JsonValue;
-import org.camunda.spin.plugin.variable.value.SpinValue;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-import sk.fiit.phutumi.models.Restaurant;
 
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.List;
 
-@Named("hello")
+@Named("printrest")
 @Service
-public class Hello implements JavaDelegate {
+public class PrintFood implements JavaDelegate {
     WebClient client = WebClient.create("http://localhost:8080/phutumi");
+
+//
     @Override
     public void execute(DelegateExecution delegateExecution){
-        Mono<List> restaurants = client.get().uri("/restaurants").retrieve().bodyToMono(List.class);
-        List resList = restaurants.block();
+        String restaurantId = delegateExecution.getVariable("input").toString();
+        System.out.println("Restaurant ID: " + restaurantId);
 
-        // ukazka json
-        //[{"label":"Burger King","value":"2"},{"label":"McDonald's","value":"1"},{"label":"Pizza Hut","value":"3"}]
+        Mono<List> foods = client.get().uri("/food?restaurantId=" + restaurantId).retrieve().bodyToMono(List.class);
+        List foodList = foods.block();
 
         String textToJson = "[";
-        for (int i=0; i<resList.size();i++){
-            String value = ((LinkedHashMap<?, ?>) resList.get(i)).get("id").toString();
-            String label = (String) ((LinkedHashMap<?, ?>) resList.get(i)).get("name");
+        for (int i=0; i<foodList.size();i++){
+            String value = ((LinkedHashMap<?, ?>) foodList.get(i)).get("id").toString();
+            String label = (String) ((LinkedHashMap<?, ?>) foodList.get(i)).get("name");
             textToJson = textToJson.concat("{ \"label\": \""+label+"\", \"value\": \""+value+"\"}");
-            if(i!=resList.size()-1){
+            if(i!=foodList.size()-1){
                 textToJson = textToJson.concat(",");
             }
         }
         textToJson = textToJson.concat("]");
-
-
         JsonValue jsonValue = SpinValues.jsonValue(textToJson).create();;
         System.out.println(jsonValue.getValue());
         delegateExecution.setVariable("address", jsonValue);
